20b25b22ce9e61c68e76620061c48f25df5e0547
Mustafa Gözcü <gozcumustafa34@gmail.com>
2024-07-10 14:23:30 +0300

    feat: Add truncateOverview function to shorten film overviews

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,6 +3,12 @@ import React from "react";
 const MovieList = (props) => {
 
 
+    const truncateOverview = (string, maxLength) => {
+        if (!string) return null;
+        if (string.length < maxLength) return string;
+        return `${string.substring(0, maxLength)}...`
+    }
+
 
         return (
             <div className="row">
@@ -12,7 +18,7 @@ const MovieList = (props) => {
                             <img src={movie.imageURL} className="card-img-top" alt="Sample Movie" />
                             <div className="card-body">
                                 <h5 className="card-title">{movie.name}</h5>
-                                <p className="card-text">{movie.overview}</p>
+                                <p className="card-text">{truncateOverview(movie.overview, 200)}</p>
                                 <div className="d-flex justify-content-between align-items-center">
                                     <button
                                         type="button"
